ddbed8d4b7858fe2931d8c254b7ee727a86e1e98
XvSvM <vadiagadia@gmail.com>
2024-10-01 21:43:41 +0300

    Update device.mk

--- device.mk
+++ device.mk
@@ -19,10 +19,10 @@ PRODUCT_PACKAGES += \
     android.hardware.boot@1.0-service
 
 PRODUCT_PACKAGES += \
-    bootctrl.diana
+    bootctrl.saturn
 
 PRODUCT_STATIC_BOOT_CONTROL_HAL := \
-    bootctrl.diana \
+    bootctrl.saturn \
     libgptutils \
     libz \
     libcutils
