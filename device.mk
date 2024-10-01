d93e23f686fe7760c449679b60c1c37a04b0cb2e
XvSvM <vadiagadia@gmail.com>
2024-10-01 21:24:23 +0300

    Update device.mk

--- device.mk
+++ device.mk
@@ -11,26 +11,25 @@ AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
     POSTINSTALL_PATH_system=system/bin/otapreopt_script \
     FILESYSTEM_TYPE_system=ext4 \
-    POSTINSTALL_OPTIONAL_system=true \
+    POSTINSTALL_OPTIONAL_system=true
 
 # Boot control HAL
 PRODUCT_PACKAGES += \
     android.hardware.boot@1.0-impl \
-    android.hardware.boot@1.0-impl.recovery \
-    android.hardware.boot@1.0-service \
+    android.hardware.boot@1.0-service
 
 PRODUCT_PACKAGES += \
-    bootctrl.diana \
-    
+    bootctrl.diana
+
 PRODUCT_STATIC_BOOT_CONTROL_HAL := \
     bootctrl.diana \
     libgptutils \
     libz \
-    libcutils \
+    libcutils
 
 PRODUCT_PACKAGES += \
     otapreopt_script \
     cppreopts.sh \
     update_engine \
     update_verifier \
-    update_engine_sideload \
+    update_engine_sideload
